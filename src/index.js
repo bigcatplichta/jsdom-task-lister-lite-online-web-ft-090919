9e7496b8901665cef50adf5fbd5f71f207b1281f
Matt Plichta <bigcatplichta@gmail.com>
2020-01-03 20:49:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const newTaskForm = document.getElementById("create-task-form");
   const newTaskDescription = document.getElementById("new-task-description");
   
+  
+  
 });
